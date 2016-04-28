46b4a8074342ab9f34862b212e0127b817a9721f
Heow Goodman <spamtrap@alphageeksinc.com>
2016-04-27 23:20:34 -0400

    good stopping point for the evening

--- src/sc/functional/website/page.clj
+++ src/sc/functional/website/page.clj
@@ -1,6 +1,10 @@
 (ns sc.functional.website.page
-  (:require [net.cgrand.enlive-html :as enlive]
-            [smeagol.contrib.util :as wiki]
+  (:require [net.cgrand.enlive-html       :as enlive]
+            [smeagol.contrib.util         :as wiki]
+            [sc.functional.website.meetup :as meetup]
+            [clj-time.core                :as time]
+            [clj-time.format              :as tformat]
+            [clj-time.local               :as tlocal]
    ))
 
 (defn camel-caseify
@@ -42,9 +46,43 @@
      [:div.arcontentcopy]             (enlive/html-content (:html wiki-article))
      )))
 
+(defn make-saying []
+  (rand-nth ["A better way to write software"
+             "Modern programming in practice"
+             "Elegant weapons for a civilized age"
+             "The best of Charleston technology"
+             "Creating amazing things"
+             ]))
+
+(defn wrap-mtg-url [s m] (str "<a target=\"_blank\" href=\"" (str (:event-url m)) "\">" s "</a>") )
+
+(defn template-home []
+  (let [wiki-article (wiki/fetch-article "FunctionalThinking")
+        meeting      (first (meetup/fetch-upcoming-meetups))
+        ]
+      (enlive/template
+   "html/home.html" []
+   [:title]                         (enlive/content (str "Functional SC: Functional Programming in the Silicon Harbor"))
+   [:h4 :span.litertxt]             (enlive/content (make-saying))
+   
+   ;; article1
+   [:div.article1.col-md-4.col-xs-12.paras :a] (enlive/set-attr :href (:title wiki-article))
+   [:div.article1.col-md-4.col-xs-12.paras :a :img.img-responsive] (enlive/set-attr :src (str "content/" (:title wiki-article) ".jpg"))
+   [:div.article1.col-md-4.col-xs-12.paras :a :span.date :strong]  (enlive/content (first (:date (:metadata wiki-article))))
+   [:div.article1.col-md-4.col-xs-12.paras :h5 :b]                 (enlive/content (make-title wiki-article))
+   [:div.article1.col-md-4.col-xs-12.paras :p.date]  (enlive/content "Concepts of functional programming is made much harder when the developer is also trying to learn a new language, like Haskell, Scala or Clojure at the same time. With that in mind, we focus on relating functional concepts in a common language: Java.")
+
+   ;; upcoming meeting
+   [:div.banner3 :div.container :div.row.features]  (enlive/html-content (wrap-mtg-url (str "Join us " (tformat/unparse (.withZone (tformat/formatter "EEEE, MMMM d, h:mm a") (time/time-zone-for-id "America/New_York")) (:time meeting))) meeting))
+   [:div.banner3 :div.container :div.row.bck]       (enlive/html-content (wrap-mtg-url (:title meeting) meeting))
+   [:div.banner3 :div.container :div.row.handcraft] (enlive/html-content (str (str "<p>" (wrap-mtg-url (apply str (rest (rest (rest (first (clojure.string/split (:description meeting) #"</p>")))))) meeting ) "</p>")) "<p>" (wrap-mtg-url "read more..." meeting) "</p>")
+   )
+    )
+)
+
 (defn home-page []
-  (slurp "src/html/home.html")
-  )
+  ;;(slurp "src/html/home.html")
+  ((template-home)))
 
 (defn article-page [wiki-article]
   ;; is it really an article or just content page?  article will have a date
