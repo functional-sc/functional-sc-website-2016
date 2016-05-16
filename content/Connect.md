5ceeb192f72a743168d3ceaf32e176d7c084a906
Heow Goodman <spamtrap@alphageeksinc.com>
2016-05-16 11:49:10 -0400

    added simple form handling

--- content/Connect.md
+++ content/Connect.md
@@ -11,7 +11,13 @@ We meet virtually at Charleston Tech Slack, here's how to login for a great grou
 Personally at [Functional SC Meetup](http://meetup.com/Functional-SC). We host monthly meetings, presentations, social and technical events.
 
 ## Question or Comment?
-Send us a message and recieve a response with 24 hours: **contact @ functional.sc**
 
+Send us a message and receive a response with 24 hours:
 
-
+<table style="border=0">
+<form method="post" accept-charset="UTF-8" name="connect" action="/connect">
+<tr><td> email</td>    <td><input value="" type="text" name="email" /></td></tr>
+<tr><td> message </td> <td><textarea rows="5" name="message" cols="40"></textarea> </td></tr>
+<tr><td></td><td><input value="send" type="submit" name="submit" /> </td></tr>
+</form>
+</table>
--- src/sc/functional/website/handler.clj
+++ src/sc/functional/website/handler.clj
@@ -3,12 +3,36 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [smeagol.contrib.util :as wiki]
-            [sc.functional.website.page :as page]))
+            [sc.functional.website.page :as page]
+            [clojure.java.shell :as shell]))
+
+
 
 (defn validate-input "Only keep specific characters in the input string."
      [input-str]
      (let [re (re-pattern "[a-zA-Z0-9\\:\\/\\-\\_\\ \\.\\?\\!\\@]")]
        (apply str (filter #(re-matches re (str %1)) input-str))))
+
+;;
+;; form handling
+;;
+
+(defn map->mailstr
+  "Given a parameter map, stringify it all parameters formatted as a mail form.  We used to call this FormMail and it's not used for a reason."
+  [params]
+  (apply str
+         (interpose "\n"
+              (map #(str (first %1) ": " (validate-input (second %1))) params))))
+
+(defn mail-generic [params email-target subject thanks-target]
+  (if (empty? (params "jobtitle")) ; botbait
+    (let [msg (map->mailstr params)
+          cmd (str "/bin/echo '" msg "' | /usr/bin/mail -s \"" subject "\" " email-target)]
+      (shell/sh "/bin/sh" "-c" cmd)
+      (str "<html><meta http-equiv=\"REFRESH\" content=\"0;url=/" thanks-target "\"></HEAD></html>") )))
+
+(defn mail-connect [params]
+  (mail-generic params "heow@alphageeksinc.com" "functional sc comment" "connect-thanks"))
 
 (defn process-wiki-or-404
   "determine and dispatch on wiki topic, or it's a 404"
@@ -27,6 +51,9 @@
   ;; publish all files in src/html as if they were root
   (route/files     "/" {:root "./src/html"}) ; standalone from root dir
   (route/resources "/" {:root "./html"})      ; war file from classes dir
+
+  ;; form processing
+  (POST "/connect" {params :params} (mail-connect params))
 
   ;; last chance, perhaps there is an article
   (GET "/*" {params :params :as request} (process-wiki-or-404 request))
