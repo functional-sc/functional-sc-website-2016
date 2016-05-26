afb09a5fc4c700feef67d7ab78c800780e9e6c92
Heow Goodman <spamtrap@alphageeksinc.com>
2016-05-25 21:37:18 -0400

    added twitter feed

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ rm target/*.war
 lein ring uberwar
 
 cp target/*.war ../functional-sc-appserver/webapps/ROOT.war
-
 echo "deployed to ../functional-sc-appserver/webapps/ROOT.war"
+
+scp target/*.war fpsc@do:~/jetty/webapps/ROOT.war
+echo "deployed digital ocean"
